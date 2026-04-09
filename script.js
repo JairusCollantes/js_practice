c56cbafed3a95064422300aa7e1ea6af4c899b06
Jairus Mark <jairuscollantes0@gmail.com>
2026-04-09 18:27:07 +0800

    restarto

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
-const target_word = "hello";
-let startTime = null;
-
-document.getElementById("target").textContent =
-  "Type the word: " + target_word;
+const target_word = "nigger";
 
 const input = document.getElementById("input");
 const result = document.getElementById("result");
+const target = document.getElementById("target");
+
+let startTime = null;
+
+function startGame() {
+  input.value = "";
+  input.disabled = false;
+  result.innerHTML = "";
+  startTime = null;
+
+  target.textContent = "Type the word: " + target_word;
+}
 
 input.addEventListener("input", () => {
   let value = input.value;
 
-  // start timer on first letter
   if (value.length === 1 && startTime === null) {
     startTime = Date.now();
   }
 
-  // stop condition
   if (value.length >= target_word.length) {
     let endTime = Date.now();
     let elapsed = (endTime - startTime) / 1000;
 
     result.innerHTML = `
       Your input: ${value} <br>
-      Target word: ${target_word} <br>
-      Time taken: ${elapsed.toFixed(3)} seconds <br>
+      Time: ${elapsed.toFixed(3)}s <br>
       ${value === target_word ? "Correct!" : "Incorrect!"}
     `;
 
     input.disabled = true;
+
+    setTimeout(startGame, 2000);
   }
 });
+
+startGame();
